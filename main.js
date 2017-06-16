227270dbda5c74280a03f13c5f4f771cf2d17d65
“mateen1222” <mateenkaziawebdev@gmail.com>
2017-06-16 09:53:28 -0500

    Clear isnt working

---
+++ main.js
@@ -0,0 +1,62 @@
+function run1(){
+    document.calculator.result.value  += "1 ";}
+function run2(){
+    document.calculator.result.value  += "2 ";}
+function run3(){
+    document.calculator.result.value  += "3 ";}
+function run4(){
+    document.calculator.result.value  += "4 ";}
+function run5(){
+        document.calculator.result.value  += "5 ";}
+function run6(){
+        document.calculator.result.value  += "6 ";}
+function run7(){
+        document.calculator.result.value  += "7 ";}
+function run8(){
+                document.calculator.result.value  += "8 ";}
+function run9(){
+                document.calculator.result.value  += "9 ";}
+function run0(){
+                document.calculator.result.value  += "0 ";}
+function runPlus(){
+                document.calculator.result.value  += "+";}
+function runMinus(){
+                document.calculator.result.value  += "-";}
+function runDivi(){
+                document.calculator.result.value  += "/";}
+function runMulti(){
+               document.calculator.result.value  += "*";}
+function runDot(){
+                document.calculator.result.value  += ".";}
+
+function runClear(){
+
+    if(document.calculator.result.value = " "){
+
+    }return " "
+
+    }
+
+
+
+
+function evalu(){
+
+
+var add = eval(document.calculator.result.value)
+
+     document.calculator.result.value = add ;
+
+var minus = eval(document.calculator.result.value)
+
+        document.calculator.result.value = minus ;
+
+
+var divi = eval(document.calculator.result.value)
+
+      document.calculator.result.value = divi ;
+
+var multi = eval(document.calculator.result.value)
+
+      document.calculator.result.value = multi ;
+}
