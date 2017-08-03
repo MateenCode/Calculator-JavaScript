f0bf0733d32e888aff740b2079fe8ebb8b1e8616
“mateen1222” <mateenkaziawebdev@gmail.com>
2017-08-02 22:34:49 -0500

    FUNCTIONALand WORKING CSS needed

--- main.js
+++ main.js
@@ -1,62 +1,47 @@
-function run1(){
-    document.calculator.result.value  += "1 ";}
-function run2(){
-    document.calculator.result.value  += "2 ";}
-function run3(){
-    document.calculator.result.value  += "3 ";}
-function run4(){
-    document.calculator.result.value  += "4 ";}
-function run5(){
-        document.calculator.result.value  += "5 ";}
-function run6(){
-        document.calculator.result.value  += "6 ";}
-function run7(){
-        document.calculator.result.value  += "7 ";}
-function run8(){
-                document.calculator.result.value  += "8 ";}
-function run9(){
-                document.calculator.result.value  += "9 ";}
-function run0(){
-                document.calculator.result.value  += "0 ";}
-function runPlus(){
-                document.calculator.result.value  += "+";}
-function runMinus(){
-                document.calculator.result.value  += "-";}
-function runDivi(){
-                document.calculator.result.value  += "/";}
-function runMulti(){
-               document.calculator.result.value  += "*";}
-function runDot(){
-                document.calculator.result.value  += ".";}
-
-function runClear(){
-
-    if(document.calculator.result.value = " "){
-
-    }return " "
-
+const zero = document.querySelector("#zero")
+const one = document.querySelector("#one")
+const two = document.querySelector("#two")
+const three = document.querySelector("#three")
+const four = document.querySelector("#four")
+const five = document.querySelector("#five")
+const six = document.querySelector("#six")
+const seven = document.querySelector("#seven")
+const eight = document.querySelector("#eight")
+const nine = document.querySelector("#nine")
+const add = document.querySelector("#add")
+const minus = document.querySelector("#minus")
+const multiply = document.querySelector("#multiply")
+const divide = document.querySelector("#divide")
+const decimal = document.querySelector("#decimal")
+const clear = document.querySelector("#clear")
+const answer = document.querySelector("#answer")
+const button = document.querySelectorAll('.button')
+const array = []
+const answerArray = []
+for(let i=0; i<button.length; i++){
+  button[i].addEventListener('click', function(){
+    if(button[i] === zero || button[i] === one || button[i] === two || button[i] === three || button[i] === four || button[i] === five || button[i] === six || button[i] === seven || button[i] === eight || button[i] === nine || button[i] === decimal || button[i] === divide || button[i] === minus || button[i] === add || button[i] === multiply){
+      answerArray.push(button[i].textContent)
+      array.push(button[i].textContent)
+      answer.textContent = answerArray.join('')
+    } else if(button[i] === multiply) {
+      answerBox.push("*")
+      array.push("*")
+      answer.textContent = answerBox.join('')
     }
-
-
-
-
-function evalu(){
-
-
-var add = eval(document.calculator.result.value)
-
-     document.calculator.result.value = add ;
-
-var minus = eval(document.calculator.result.value)
-
-        document.calculator.result.value = minus ;
-
-
-var divi = eval(document.calculator.result.value)
-
-      document.calculator.result.value = divi ;
-
-var multi = eval(document.calculator.result.value)
-
-      document.calculator.result.value = multi ;
+      else if (button[i] === equals) {
+        const final = eval(array.join(''))
+          answer.textContent = final
+          array.length = 0
+          answerArray.length = 0
+          array.push(answer.textContent)
+          answerArray.push(answer.Content)
+      }
+      else if (button[i] === clear) {
+        array.length = 0
+        answerArray.length = 0
+        answer.textContent = ''
+        return array + answerArray
+      }
+  })
 }
