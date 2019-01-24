55efc6c56c1435e2cb42b5bf5956afaf1b7ccd6a
Mateen kazia <mateenkazia@Mateens-MacBook-Pro.local>
2019-01-24 09:03:04 -0800

    updated and working more faster

--- main.js
+++ main.js
@@ -1,47 +1,131 @@
-const zero = document.querySelector("#zero")
-const one = document.querySelector("#one")
-const two = document.querySelector("#two")
-const three = document.querySelector("#three")
-const four = document.querySelector("#four")
-const five = document.querySelector("#five")
-const six = document.querySelector("#six")
-const seven = document.querySelector("#seven")
-const eight = document.querySelector("#eight")
-const nine = document.querySelector("#nine")
-const add = document.querySelector("#add")
-const minus = document.querySelector("#minus")
-const multiply = document.querySelector("#multiply")
-const divide = document.querySelector("#divide")
-const decimal = document.querySelector("#decimal")
-const clear = document.querySelector("#clear")
-const answer = document.querySelector("#answer")
-const button = document.querySelectorAll('.button')
-const array = []
-const answerArray = []
-for(let i=0; i<button.length; i++){
-  button[i].addEventListener('click', function(){
-    if(button[i] === zero || button[i] === one || button[i] === two || button[i] === three || button[i] === four || button[i] === five || button[i] === six || button[i] === seven || button[i] === eight || button[i] === nine || button[i] === decimal || button[i] === divide || button[i] === minus || button[i] === add || button[i] === multiply){
-      answerArray.push(button[i].textContent)
-      array.push(button[i].textContent)
-      answer.textContent = answerArray.join('')
-    } else if(button[i] === multiply) {
-      answerBox.push("*")
-      array.push("*")
-      answer.textContent = answerBox.join('')
+"use strict";
+
+var input = document.getElementById("input"), // input/output button
+  number = document.querySelectorAll(".numbers div"), // number buttons
+  operator = document.querySelectorAll(".operators div"), // operator buttons
+  result = document.getElementById("result"), // equal button
+  clear = document.getElementById("clear"), // clear button
+  resultDisplayed = false; // flag to keep an eye on what output is displayed
+
+// adding click handlers to number buttons
+for (var i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function(e) {
+    // storing current input string and its last character in variables - used later
+    var currentString = input.innerHTML;
+    var lastChar = currentString[currentString.length - 1];
+
+    // if result is not diplayed, just keep adding
+    if (resultDisplayed === false) {
+      input.innerHTML += e.target.innerHTML;
+    } else if (
+      (resultDisplayed === true && lastChar === "+") ||
+      lastChar === "-" ||
+      lastChar === "×" ||
+      lastChar === "÷"
+    ) {
+      // if result is currently displayed and user pressed an operator
+      // we need to keep on adding to the string for next operation
+      resultDisplayed = false;
+      input.innerHTML += e.target.innerHTML;
+    } else {
+      // if result is currently displayed and user pressed a number
+      // we need clear the input string and add the new input to start the new opration
+      resultDisplayed = false;
+      input.innerHTML = "";
+      input.innerHTML += e.target.innerHTML;
     }
-      else if (button[i] === equals) {
-        const final = eval(array.join(''))
-          answer.textContent = final
-          array.length = 0
-          answerArray.length = 0
-          array.push(answer.textContent)
-          answerArray.push(answer.Content)
-      }
-      else if (button[i] === clear) {
-        array.length = 0
-        answerArray.length = 0
-        answer.textContent = ''
-        return array + answerArray
-      }
-  })
+  });
 }
+
+// adding click handlers to number buttons
+for (var i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", function(e) {
+    // storing current input string and its last character in variables - used later
+    var currentString = input.innerHTML;
+    var lastChar = currentString[currentString.length - 1];
+
+    // if last character entered is an operator, replace it with the currently pressed one
+    if (
+      lastChar === "+" ||
+      lastChar === "-" ||
+      lastChar === "×" ||
+      lastChar === "÷"
+    ) {
+      var newString =
+        currentString.substring(0, currentString.length - 1) +
+        e.target.innerHTML;
+      input.innerHTML = newString;
+    } else if (currentString.length == 0) {
+      // if first key pressed is an opearator, don't do anything
+      console.log("enter a number first");
+    } else {
+      // else just add the operator pressed to the input
+      input.innerHTML += e.target.innerHTML;
+    }
+  });
+}
+
+// on click of 'equal' button
+result.addEventListener("click", function() {
+  // this is the string that we will be processing eg. -10+26+33-56*34/23
+  var inputString = input.innerHTML;
+
+  // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
+  var numbers = inputString.split(/\+|\-|\×|\÷/g);
+
+  // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
+  // first we replace all the numbers and dot with empty string and then split
+  var operators = inputString.replace(/[0-9]|\./g, "").split("");
+
+  console.log(inputString);
+  console.log(operators);
+  console.log(numbers);
+  console.log("----------------------------");
+
+  // now we are looping through the array and doing one operation at a time.
+  // first divide, then multiply, then subtraction and then addition
+  // as we move we are alterning the original numbers and operators array
+  // the final element remaining in the array will be the output
+
+  var divide = operators.indexOf("÷");
+  while (divide != -1) {
+    numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+    operators.splice(divide, 1);
+    divide = operators.indexOf("÷");
+  }
+
+  var multiply = operators.indexOf("×");
+  while (multiply != -1) {
+    numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+    operators.splice(multiply, 1);
+    multiply = operators.indexOf("×");
+  }
+
+  var subtract = operators.indexOf("-");
+  while (subtract != -1) {
+    numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+    operators.splice(subtract, 1);
+    subtract = operators.indexOf("-");
+  }
+
+  var add = operators.indexOf("+");
+  while (add != -1) {
+    // using parseFloat is necessary, otherwise it will result in string concatenation :)
+    numbers.splice(
+      add,
+      2,
+      parseFloat(numbers[add]) + parseFloat(numbers[add + 1])
+    );
+    operators.splice(add, 1);
+    add = operators.indexOf("+");
+  }
+
+  input.innerHTML = numbers[0]; // displaying the output
+
+  resultDisplayed = true; // turning flag if result is displayed
+});
+
+// clearing the input on press of clear
+clear.addEventListener("click", function() {
+  input.innerHTML = "";
+});
